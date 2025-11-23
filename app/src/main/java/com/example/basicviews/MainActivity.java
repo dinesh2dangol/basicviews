738c4e7aa469581c81775302a7308bc33eb60e14
Dinesh Dangol <dines.dangol@gmail.com>
2025-11-23 16:57:03 +0545

    Options menu updated

--- app/src/main/java/com/example/basicviews/MainActivity.java
+++ app/src/main/java/com/example/basicviews/MainActivity.java
@@ -58,10 +58,23 @@ public class MainActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+        NavController navController =
+                Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Snackbar.make(binding.getRoot(), "Settings clicked", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (id == R.id.action_refresh) {
+            Snackbar.make(binding.getRoot(), "Refreshing...", Snackbar.LENGTH_SHORT).show();
+            return true;
+        }
+        else if (id == R.id.action_about) {
+            navController.navigate(R.id.ThirdFragment);  // <-- navigate here!
+            Snackbar.make(binding.getRoot(), "BasicViews App v1.0", Snackbar.LENGTH_SHORT).show();
             return true;
         }
 
