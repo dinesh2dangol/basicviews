c60107053fce9762de3dfe6ce3ebe8cdd903c472
Dinesh Dangol <dines.dangol@gmail.com>
2025-11-23 15:41:37 +0545

    Third Fragment added.

--- app/src/main/java/com/example/basicviews/ThirdFragment.java
+++ app/src/main/java/com/example/basicviews/ThirdFragment.java
@@ -9,11 +9,11 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
-import com.example.basicviews.databinding.FragmentSecondBinding;
+import com.example.basicviews.databinding.FragmentThirdBinding;
 
-public class SecondFragment extends Fragment {
+public class ThirdFragment extends Fragment {
 
-    private FragmentSecondBinding binding;
+    private FragmentThirdBinding binding;
 
     @Override
     public View onCreateView(
@@ -21,7 +21,7 @@ public class SecondFragment extends Fragment {
             Bundle savedInstanceState
     ) {
 
-        binding = FragmentSecondBinding.inflate(inflater, container, false);
+        binding = FragmentThirdBinding.inflate(inflater, container, false);
         return binding.getRoot();
 
     }
@@ -29,9 +29,9 @@ public class SecondFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonSecond.setOnClickListener(v ->
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_ThirdFragment)
+        binding.buttonThird.setOnClickListener(v ->
+                NavHostFragment.findNavController(ThirdFragment.this)
+                        .navigate(R.id.action_ThirdFragment_to_FirstFragment)
         );
     }
 
